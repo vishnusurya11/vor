0f2d6f1853dbf07773e091ff78b4dc008c7e07c7
vishnusurya11 <vishnusurya11@google.com>
2019-12-07 10:54:22 -0800

    added logging for errors

--- scripts/vor.py
+++ scripts/vor.py
@@ -1,7 +1,20 @@
-# Main Program
+"""Main Program"""
 
 # Libraries
+import time
+import logging
 
+# Logging
+logging.basicConfig(
+    filename='vor_error.log',
+    level=logging.INFO,
+    format='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S %Z'
+)
 
 if __name__ == '__main__':
-	print("Hello")
+    try:
+        time.sleep(10)
+        print("Hello")
+    except KeyboardInterrupt:
+        print("vor is terminated")
