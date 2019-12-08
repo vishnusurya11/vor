0b46fcc185a5424670af2964eb872daf8593c292
vishnusurya11 <vishnusurya11@google.com>
2019-12-07 16:20:32 -0800

    added vor main function and folder_check functions

--- scripts/vor.py
+++ scripts/vor.py
@@ -4,6 +4,32 @@
 import time
 import logging
 
+
+
+def vor():
+	""" Main function that calls other functions to collect data and write
+	to a given location
+	"""
+	try:
+		print('Hola')
+		time.sleep(10)
+		folder_check()
+		return True
+	except Exception as e:
+		logging.exception(e)
+		return False
+
+def folder_check():
+	"""Checks the folder structure and files required to run vor
+	and creates if not available
+	"""
+	try:
+		print('folder check function ran successfully')
+		return True
+	except Exception as e:
+		logging.exception(e)
+		return False
+
 # Logging
 logging.basicConfig(
     filename='vor_error.log',
@@ -13,8 +39,7 @@ logging.basicConfig(
 )
 
 if __name__ == '__main__':
-    try:
-        time.sleep(10)
-        print("Hello")
-    except KeyboardInterrupt:
-        print("vor is terminated")
+	try:
+		vor()
+	except KeyboardInterrupt:
+		print("vor is terminated")
