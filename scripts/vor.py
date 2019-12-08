c8fb93781392ca0de8051a00b18e4bae1643e8a0
vishnusurya11 <vishnusurya11@google.com>
2019-12-07 16:46:38 -0800

    changed tabs to spaces

--- scripts/vor.py
+++ scripts/vor.py
@@ -7,28 +7,28 @@ import logging
 
 
 def vor():
-	""" Main function that calls other functions to collect data and write
-	to a given location
-	"""
-	try:
-		print('Hola')
-		time.sleep(10)
-		folder_check()
-		return True
-	except Exception as e:
-		logging.exception(e)
-		return False
+    """ Main function that calls other functions to collect data and write
+    to a given location
+    """
+    try:
+        print('Hola')
+        time.sleep(10)
+        folder_check()
+        return True
+    except Exception as e:
+        logging.exception(e)
+        return False
 
 def folder_check():
-	"""Checks the folder structure and files required to run vor
-	and creates if not available
-	"""
-	try:
-		print('folder check function ran successfully')
-		return True
-	except Exception as e:
-		logging.exception(e)
-		return False
+    """Checks the folder structure and files required to run vor
+    and creates if not available
+    """
+    try:
+        print('folder check function ran successfully')
+        return True
+    except Exception as e:
+        logging.exception(e)
+        return False
 
 # Logging
 logging.basicConfig(
@@ -39,7 +39,7 @@ logging.basicConfig(
 )
 
 if __name__ == '__main__':
-	try:
-		vor()
-	except KeyboardInterrupt:
-		print("vor is terminated")
+    try:
+        vor()
+    except KeyboardInterrupt:
+        print("vor is terminated")
