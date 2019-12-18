e3b8a22962ea57526e7a2b6a6f8ad36d2143ec3d
vishnusurya11 <vishnusurya11@gmail.com>
2019-12-17 23:23:24 -0800

    added program to create process_log

---
+++ scripts/get_list_of_files.py
@@ -0,0 +1,41 @@
+import pandas
+
+'''
+stock_df = pandas.read_csv('NYSE.txt', sep = '\t')
+
+print(stock_df)'''
+
+def clean_data():
+    with open('NYSE.txt', 'r') as f:
+        a = f.readlines()
+    a = a[1:]
+    with open('NYSE_new.txt', 'w') as j:
+        for x in a:
+            if '-' in x or '.' in x:
+                continue
+            j.write(x)
+    return True
+
+
+def create_process_log():
+    with open('process_log.csv', 'w') as f:
+        f.write('Symbol,Description,Status,Daily_Stock_Data_Created_Time,Daily_Stock_Data_Updated_Time,Minute_Stock_Data_Created_Time,Minute_Stock_Data_Updated_Time\n')
+    return True
+
+def add_to_process_log():
+    with open('NYSE_new.txt', 'r') as f:
+        a = f.readlines()
+
+    with open('process_log.csv', 'a') as j:
+        try:
+            for x in a:
+                j.write(','.join(x.replace('\n','').split('\t'))+',0,,,,\n')
+        except Exception as e:
+            print(e)
+    
+    return True
+
+if __name__ == '__main__':
+    #clean_data()
+    create_process_log()
+    add_to_process_log()
