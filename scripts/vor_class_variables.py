76a110aec84907003ef34ee0dfb24d65500ea24d
vishnusurya11 <vishnusurya11@gmail.com>
2019-12-25 11:24:50 -0800

    added processlogentry class object

---
+++ scripts/vor_class_variables.py
@@ -0,0 +1,9 @@
+class ProcessLogEntry():
+    def __init__(self):
+        self.symbol = None
+        self.description = None
+        self.status = None
+        self.daily_stock_data_created_time = None
+        self.daily_stock_data_updated_time = None
+        self.minute_stock_data_created_time = None
+        self.minute_stock_data_updated_time = None
